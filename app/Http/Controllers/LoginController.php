9d071bcf1efe7ae03efd2004d1a9bd9d4822e4ab
Antoine07 <antoine.lucsko@gmail.com>
2018-05-02 07:08:13 +0200

    login correction redirection

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -21,6 +21,6 @@ class LoginController extends Controller
             return redirect()->intended('dashboard');
         }
 
-        dump(Auth::attempt($credentials));
+       //  dump(Auth::attempt($credentials));
     }
 }
